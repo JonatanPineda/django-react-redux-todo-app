407be28f1558c370d4076a59b3f69357babbeac9
Jonatan Pineda Perez <j.pineda.p1@gmail.com>
2019-01-24 03:36:32 -0700

    Setup UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Layout, Input, Button, List, Icon } from "antd";
+import Todo from "./models/Todo"
+
+import "./App.css";
+
+const { Header, Footer, Content } = Layout;
 
 class App extends Component {
+  state = {
+    addingTodo: false,
+    pendingTodo: "",
+    todos: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/todos/");
+      const todos = await res.json();
+      this.setState({
+        todos
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
+  addTodo() {}
+
+  completeTodo(id: any) {}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout className="App">
+        <Header className="App-header">
+          <h1>Todos</h1>
+        </Header>
+        <Content className="App-content">
+          <Input
+              ref="add-todo-input"
+              className="App-add-todo-input"
+              size="large"
+              placeholder="What needs to be done?"
+              disabled={this.state.addingTodo}
+              onChange={evt => this.setState({ pendingTodo: evt.target.value })}
+              value={this.state.pendingTodo}
+              onPressEnter={this.addTodo}
+            />
+            <Button 
+              className="App-add-todo-button"
+              size="large"
+              type="primary"
+              onClick={this.addTodo}
+              loading={this.state.addingTodo}
+            >
+            Add Todo
+          </Button>
+          <List
+            className="App-todos"
+            size="large"
+            bordered
+            dataSource={this.state.todos}
+            renderItem={(todo: Todo)  => (
+              <List.Item>
+                {todo.content}
+                <Icon
+                  onClick={evt => this.completeTodo(todo.id)}
+                  className="App-todo-complete"
+                  type="check"
+                />
+              </List.Item>
+            )}
+          />
+        </Content>
+        <Footer className="App-footer">&copy; https://github.com/JonatanPineda/django-react-redux-todo-app</Footer>
+      </Layout>
     );
   }
 }
